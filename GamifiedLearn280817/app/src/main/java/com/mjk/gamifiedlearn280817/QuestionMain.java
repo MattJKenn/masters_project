f797b281ff1cdb824e03b2427a1e121d86d75aea
Matthew Kennedy <mattjkenn@gmail.com>
2017-08-29 14:29:36 +0100

    setup intent

--- GamifiedLearn280817/app/src/main/java/com/mjk/gamifiedlearn280817/QuestionMain.java
+++ GamifiedLearn280817/app/src/main/java/com/mjk/gamifiedlearn280817/QuestionMain.java
@@ -3,8 +3,10 @@ package com.mjk.gamifiedlearn280817;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 public class QuestionMain extends AppCompatActivity {
@@ -17,6 +19,16 @@ public class QuestionMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_main);
+
+        TextView textView = (TextView) findViewById(R.id.quiz_textView);
+
+        Bundle bundle = getIntent().getExtras();
+        String message = bundle.getString("test");
+        Log.d("Quiz",message);
+
+        if (message !=  null) {
+            textView.setText(message);
+        }
     }
 
 
